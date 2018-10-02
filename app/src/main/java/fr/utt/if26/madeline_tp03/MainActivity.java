636e60315146594a8f970a2b9ee7e6c8770871ed
if26-grp1 <if26-grp1@mac05-m102.utt.fr>
2018-10-02 15:17:33 +0200

    Initial commit

---
+++ app/src/main/java/fr/utt/if26/madeline_tp03/MainActivity.java
@@ -0,0 +1,33 @@
+package fr.utt.if26.madeline_tp03;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Super méthode qui créerun bouton qui permet de changer un texte de couleur. Amazing.
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView tx = (TextView) findViewById(R.id.textview);
+        String str = (String) getResources().getString(R.string.message);
+        tx.setText(str);
+
+        Button btn = (Button) findViewById(R.id.bouton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               tx.setBackgroundColor(Color.RED);
+            }
+        });
+
+    }
+}
